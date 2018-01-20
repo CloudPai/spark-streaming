ca88f3224caa8a32b9c9408172a31aa5a9ebd9be
cloudPai <1565730756@qq.com>
2018-01-20 21:55:49 +0800

    spark streaming 实时流处理  第4章 分布式发布订阅消息系统Kafka 完成 backup

---
+++ src/main/java/com/imooc/spark/kafka/KafkaProperties.java
@@ -0,0 +1,16 @@
+package com.imooc.spark.kafka;
+/**
+ * Kafka常用配置文件
+ */
+public class KafkaProperties {
+
+    public static final String ZK = "hadoop000:2181";
+
+    public static final String TOPIC = "hello_topic";
+
+    public static final String BROKER_LIST = "hadoop000:9092";
+
+
+    public static final String GROUP_ID = "test_group1";
+
+}
